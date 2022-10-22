c5427b1b253783f5728731e72e1aef3752f3ad26
syhrulhkim <89004999+syhrulhkim@users.noreply.github.com>
2022-10-22 14:54:23 +0800

    add: add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,15 @@ module.exports = {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Poppins', ...defaultTheme.fontFamily.sans],
             },
         },
     },
